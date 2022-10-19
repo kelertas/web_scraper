4311a4e161f0fa9de3c6a0ce511d314a376e6944
[Edgaras Kelertas] <[edgaras.kelertas@gmail.com]>
2022-10-19 11:12:43 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup( name='webscraper',
+       version='0.0.1',
+       description='Simple web scraper using beautifulsoup4 and ML techniques',
+       author="Edgaras Kelertas <edgaras.kelertas@gmail.com>",
+       packages=['webscraper']
+)
